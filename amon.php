f0eb7c206851cd9d882870b1db89bc35f6139412
AbdullahDoğan <92158614+abdullahdogan06@users.noreply.github.com>
2022-05-07 19:15:27 +0300

    Add files via upload

---
+++ amon.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+<title> AmonPhp </title>
+</head>
+<body>
+<?php
+echo"Benim adım Amon. Php öğreniyorum.. ";
+
+print("otomatik yazdırma işlemi başarılıdıor..")
+?>
+</body>
+</html>
+
